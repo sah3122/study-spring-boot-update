cb01704c1a5446dc94eb2195fc17ddcfa643e8da
dean <sah3122@naver.com>
2020-10-19 22:05:11 +0900

    first commit

---
+++ src/main/java/me/study/springbootupdate/SpringBootUpdateApplication.java
@@ -0,0 +1,13 @@
+package me.study.springbootupdate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUpdateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootUpdateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/study/springbootupdate/SpringBootUpdateApplicationTests.java
@@ -0,0 +1,13 @@
+package me.study.springbootupdate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootUpdateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
